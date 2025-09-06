d4fb3e749e1cd98ae1de46094c855f305f2420f7
agodahdev Goddah <godahahmed@gmail.com>
2025-09-06 11:58:05 +0000

    add minmul stubs to start app

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from app_pages.page_1_summary import run as page_summary
+from app_pages.page_2_analysis import run as page_analysis
+from app_pages.page_3_hypotheses import run as page_hypotheses
+from app_pages.page_4_ml import run as page_ml
+
+st.set_page_config(page_title="AttriSight", layout="wide")
+PAGES = {
+    "Project Summary": page_summary,
+    "Workforce Analysis": page_analysis,
+    "Project Hypotheses": page_hypotheses,
+    "Attrition Predictor (ML)": page_ml,
+}
+st.sidebar.title("AttriSight")
+choice = st.sidebar.radio("Go to", list(PAGES.keys()))
+PAGES[choice]()
